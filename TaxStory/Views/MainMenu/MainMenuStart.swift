d46004a3ae9b694a8ec72881ba8130bf2cd2e62d
LexzKuy <alexsanderlim1@gmail.com>
2023-06-23 12:26:38 +0700

    Animation and button done

--- TaxStory/Views/MainMenu/MainMenuStart.swift
+++ TaxStory/Views/MainMenu/MainMenuStart.swift
@@ -8,61 +8,87 @@
 import SwiftUI
 
 struct MainMenuStart: View {
+    @State private var rotationAngle: Angle = .degrees(0)
+    @State private var isButtonPressed = false
+    @State private var animationCount = 0
+    @State var hold = false
     var body: some View {
-        ZStack {
-            // Background image
-            Image("background")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-            
-            // SunShine
-            Image("shine")
-                .resizable()
-                .scaledToFit()
-            
-            // Building image
-            Image("building")
-                .resizable()
-                .scaledToFit()
-            
-            // Tittle
-            Image("tittle")
-                .resizable()
-                .scaledToFit()
-
-            
-//            VStack {
-//                Text("Menu")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.white)
-//                    .padding()
-//
-//                Button(action: {
-//                    // Start game action
-//                    startGame()
-//                }) {
-//                    Text("Start Game")
-//                        .font(.title)
-//                        .foregroundColor(.white)
-//                        .padding()
-//                        .background(Color.blue)
-//                        .cornerRadius(10)
-//                }
-//            }
+        // Background image
+        Image("background")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+            .overlay {
+                VStack{
+                    // SunShine
+                    Image("shine")
+                        .resizable()
+                        .scaledToFill()
+                        .rotationEffect(rotationAngle)
+                        .onAppear {withAnimation(Animation.linear(duration: 20).repeatForever(autoreverses: false)) {rotationAngle = .degrees(360)
+                        }
+                        }
+                        .frame(width: Constants.screenWidth * 1, height: Constants.screenWidth * 3)
+                }
+                
+                VStack{
+                    // Building image
+                    Image("building")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                VStack{
+                    // Tittle
+                    Image("tittle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: Constants.screenWidth * 1, height: Constants.screenWidth * 0.37)
+                        .padding(.bottom, Constants.screenHeight * 0.45)
+                }
+                
+                VStack {
+                            Button(action: {
+                                // Start game action
+                                withAnimation(.interpolatingSpring(stiffness: 300, damping: 10)) {
+                                    startGame()
+                                    isButtonPressed = true // Set isButtonPressed to true when the button is pressed
+                                }
+                                
+                                // Reset isButtonPressed after a delay
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    withAnimation {
+                                        isButtonPressed = false
+                                    }
+                                }
+                            }) {
+                                Image("StartButton")
+                                    .resizable()
+                                    .renderingMode(.original)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: Constants.screenWidth * 0.4, height: Constants.screenWidth * 0.37)
+                                    .padding(.top, Constants.screenHeight * 0.5)
+                                    .scaleEffect(isButtonPressed ? 0.9 : 1.0) // Apply scale effect based on button press
+                            }
+                            .animation(.spring()) // Apply animation to the button press
+                        }            }
         }
     }
+        
+    
+    
+    
     
     func startGame() {
         // Code to start the game
         print("Starting the game...")
     }
-}
+    
+    
 
-
-struct MainMenuStart_Previews: PreviewProvider {
-    static var previews: some View {
-        MainMenuStart()
-        .previewInterfaceOrientation(.landscapeLeft)
+    struct MainMenuStart_Previews: PreviewProvider {
+        static var previews: some View {
+            MainMenuStart()
+                .previewInterfaceOrientation(.landscapeLeft)
+        }
     }
-}
