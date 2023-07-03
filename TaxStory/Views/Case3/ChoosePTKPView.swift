de2be6d2ed9cd88ee7a89ea0e2493bd6b349e7d0
TimothyKristanto <66807730+TimothyKristanto@users.noreply.github.com>
2023-07-03 14:54:17 +0700

    Fixed bug and added new file
    - Fixed bug in calculator number formatting
    
    - Created new swift ui file to choose PTKP

---
+++ TaxStory/Views/Case3/ChoosePTKPView.swift
@@ -0,0 +1,21 @@
+//
+//  ChoosePTKPView.swift
+//  TaxStory
+//
+//  Created by Timothy on 03/07/23.
+//
+
+import SwiftUI
+
+struct ChoosePTKPView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChoosePTKPView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChoosePTKPView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
