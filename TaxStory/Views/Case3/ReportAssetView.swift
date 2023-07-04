893cf98d53a849858bf4b62ee40fe0a4fb6abb45
Verrell <sutantoverrell@gmail.com>
2023-07-04 08:51:40 +0700

    Created report asset view

---
+++ TaxStory/Views/Case3/ReportAssetView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportAssetView.swift
+//  TaxStory
+//
+//  Created by Verrell Sutanto on 04/07/23.
+//
+
+import SwiftUI
+
+struct ReportAssetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReportAssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportAssetView()
+    }
+}
