0a4d4f9d870b1d0a91fe29a0cd241b90d5af6a46
LexzKuy <alexsanderlim1@gmail.com>
2023-07-10 17:40:22 +0700

    Positioning asset done (not yet combinge jason and design)

--- TaxStory/Views/VisualNovel/VisualNovel.swift
+++ TaxStory/Views/VisualNovel/VisualNovel.swift
@@ -10,21 +10,124 @@ import SwiftUI
 
 struct VisualNovel: View {
     @State var emotions: [Emotion] = []
-    @ObservedObject var datas = ReadData()
+    @ObservedObject var dates = ReadData()
     var body: some View {
-        List(datas.emotions){ emotion in
-            
-            VStack(alignment: .leading) {
-    
-                Text(emotion.pictureName ?? "")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.gray)
-                
-                HStack{
-                    Text(emotion.textBox)
-                        .font(.title3)
-                        .foregroundColor(Color.red)
+        ZStack{
+            GeometryReader{ geoScreen in
+                VStack{
+                    Image("backgroundNovel")
+                        .resizable()
+                        .frame(width: geoScreen.size.width / 1, height: geoScreen.size.height / 0.95)
+                        .padding(.top, -geoScreen.size.height / 40)
+                        .scaledToFill()
+                        .overlay{
+                            VStack{
+                                HStack{
+                                    Button{
+                                        // munculin dialog, trus balik ke main map
+                                    }label: {
+                                        Image("Map")
+                                            .resizable()
+                                            .scaledToFit()
+                                    }
+                                    .frame(width: geoScreen.size.width / 13)
+                                    
+                                    Spacer()
+                                }
+                                .padding(.leading, geoScreen.size.width / 28)
+                                .padding(.trailing, geoScreen.size.width / 10)
+                                .padding(.top, geoScreen.size.height / 100)
+                                
+                                Spacer()
+                            }
+                        }
+                }
+                //            .offset(x: geoScreen.size.height / 1)
+                //            .offset(y: -geoScreen.size.height / 6)
+                GeometryReader{ geoBot in
+                    VStack{
+                        // guide text
+                        HStack{
+                            Image("bayuSenyum")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: geoBot.size.width / 1, height: geoBot.size.height / 1.2)
+                            //                                .offset(x: -geoBot.size.height / 1.75)
+                                .offset(y: geoScreen.size.height / 10)
+                        }
+                    }
+                    VStack{
+                        HStack{
+                            Image("textBox")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: geoBot.size.width / 1.2, height: geoBot.size.height / 2)
+                                .overlay(
+                                    Text("Yuk bantu Bayu memilih form laporan SPT (Surat Pemberitahuan Tahunan) berdasarkan perhitungan penghasilannya tahun ini menggunakan kalkulator di atas ya. Pendapatan Bayu per bulannya sendiri adalah Rp 4,500,000.")
+                                        .font(.system(size: geoBot.size.width / 65, design: .rounded))
+                                        .bold()
+                                        .padding(.horizontal, geoBot.size.width / 18)
+                                        .padding(.top, geoBot.size.height / 100)
+                                )
+                                .overlay{
+                                    VStack
+                                    {
+                                        // Building image
+                                        Button(action:
+                                                {
+                                        })
+                                        {Image("NextButton")
+                                                .resizable()
+                                                .renderingMode(.original)
+                                                .aspectRatio(contentMode: .fit)
+                                                .scaledToFit()
+                                                .frame(width: geoBot.size.width/20)
+                                            
+                                        }
+                                        .offset(y: geoBot.size.height / 20)
+                                        .offset(x: geoBot.size.height / 1.95)
+                                    }
+                                }
+                        }
+                        .padding(.top, geoBot.size.height / 1.8)
+                        .padding(.leading, geoBot.size.width / 13)
+                    }
+                    
+                    
+                    
+                    //        List(dates.emotions)
+                    //        { emotion in
+                    //            VStack(alignment: .leading) {
+                    //
+                    //                Text(emotion.pictureName ?? "")
+                    //                    .font(.title)
+                    //                    .fontWeight(.heavy)
+                    //                    .foregroundColor(Color.gray)
+                    //
+                    //                HStack{
+                    //                    Text(emotion.textBox)
+                    //                        .font(.title3)
+                    //                        .foregroundColor(Color.red)
+                    //                }
+                    //            }
+                    //        }
+                    
+                    //            ini jadi simpan
+                    //            let emotion = dates.emotions[0]
+                    //            VStack(alignment: .leading)
+                    //            {
+                    //                Text(emotion.pictureName ?? "")
+                    //                    .font(.title)
+                    //                    .fontWeight(.heavy)
+                    //                    .foregroundColor(Color.gray)
+                    //
+                    //                HStack{
+                    //                    Text(emotion.textBox)
+                    //                        .font(.title3)
+                    //                        .foregroundColor(Color.red)
+                    //
+                    //                }
+                    //            }
                 }
             }
         }
