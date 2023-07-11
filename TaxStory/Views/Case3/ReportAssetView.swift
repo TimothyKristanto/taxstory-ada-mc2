8ac7eaef90d23e7135c23407ced0458563447a9c
Verrell <sutantoverrell@gmail.com>
2023-07-11 08:15:44 +0700

    Lapor Harta Page
    Modify lapor harta view, nambahi file modal buat 4 assets, dan nambahin 2 folder buat aset lapor harta feature

--- TaxStory/Views/Case3/ReportAssetView.swift
+++ TaxStory/Views/Case3/ReportAssetView.swift
@@ -8,13 +8,285 @@
 import SwiftUI
 
 struct ReportAssetView: View {
+    @State var modalPresentTanah = false
+    @State var modalPresentRumah = false
+    @State var modalPresentMobil = false
+    @State var modalPresentMotor = false
+    
+    @State var listGambar:[String] = []
+    
+    @State var modalTanahAppended = false
+    @State var modalRumahAppended = false
+    @State var modalMobilAppended = false
+    @State var modalMotorAppended = false
+    
+//    let gridItems = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geoScreen in
+            Image("bg")
+                .resizable()
+                .scaledToFill()
+                .overlay{
+                    VStack{
+                        HStack{
+                            Button{
+                                // munculin dialog, trus balik ke main map
+                            }label: {
+                                Image("Map-2")
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            .frame(width: geoScreen.size.width / 18)
+                            
+                            Spacer()
+                            
+                            Button{
+                                // kasi hint minigames ini suruh ngapain
+                            }label: {
+                                Image("Hint")
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            .frame(width: geoScreen.size.width / 18)
+                        }
+                        .padding(.leading, geoScreen.size.width / 11)
+                        .padding(.trailing, geoScreen.size.width / 10)
+                        .padding(.top, geoScreen.size.height / 12)
+                        
+                        Spacer()
+                    }
+                }
+                .overlay{
+                    GeometryReader{ geoTop in
+                        VStack{
+                            VStack{
+                                Image("Quest")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geoTop.size.width / 5.8)
+                            }
+                            .overlay(
+                                VStack{
+                                    Text("Quest")
+                                        .font(.system(size: geoTop.size.width / 60, design: .rounded))
+                                        .bold()
+                                        .foregroundColor(.white)
+                                        .padding(.top, geoTop.size.height / 130)
+                                    
+                                    Spacer()
+                                    
+                                    VStack(alignment: .leading){
+                                        HStack{
+                                            Image(systemName: "checkmark.circle.fill")
+                                            
+                                            Text("Pilih form")
+                                                .font(.system(size: geoTop.size.width / 55, design: .rounded))
+                                        }
+                                        
+                                        HStack{
+                                            Image(systemName: "checkmark.circle.fill")
+                                            
+                                            Text("Minta bukti potong")
+                                                .font(.system(size: geoTop.size.width / 55, design: .rounded))
+                                        }
+                                        
+                                        HStack{
+                                            Image(systemName: "checkmark.circle.fill")
+                                            
+                                            Text("Isi formulir")
+                                                .font(.system(size: geoTop.size.width / 55, design: .rounded))
+                                        }
+                                        
+                                        HStack {
+                                            Image(systemName: "circle.fill")
+                                            Text("Lapor Harta")
+                                                .font(.title2)
+                                        }
+                                        
+                                        HStack{
+                                            Image(systemName: "circle")
+                                            
+                                            Text("PTKP")
+                                                .font(.system(size: geoTop.size.width / 55, design: .rounded))
+                                        }
+                                        
+                                        HStack{
+                                            Image(systemName: "circle")
+                                            
+                                            Text("Result")
+                                                .font(.system(size: geoTop.size.width / 55, design: .rounded))
+                                        }
+                                    }
+                                    .padding(.horizontal, geoTop.size.width / 30)
+                                    .padding(.top, geoTop.size.height / 50)
+                                    
+                                    Spacer()
+                                }
+                            )
+                            .offset(y: geoTop.size.height / 5.5)
+                            .offset(x: geoTop.size.width / 12)
+                            
+                            Spacer()
+                        }
+                    }
+                }
+                .overlay{
+                    GeometryReader { geoMid in
+                        
+                        Image("CanvasLaporHarta")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geoMid.size.width / 1.53, height: geoMid.size.height / 1)
+                            .offset(x: geoMid.size.width / 3.8, y: geoMid.size.height / 30)
+                        
+                        Button{
+                            modalPresentTanah = true
+                        }label: {
+                            Image("Tanah")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: geoMid.size.width / 3.1)
+                        .offset(x: geoMid.size.width / 3.58, y: geoMid.size.height / 2.6)
+                        .disabled(modalTanahAppended)
+                        .opacity(modalTanahAppended == true ? 0.5 : 1)
+                        
+                        Button{
+                            modalPresentRumah = true
+                        }label: {
+                            Image("Rumah")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: geoMid.size.width / 4)
+                        .offset(x: geoMid.size.width / 3, y: geoMid.size.height / 3.3)
+                        .disabled(modalRumahAppended)
+                        .opacity(modalRumahAppended == true ? 0.5 : 1)
+                        
+                        //                        Button{
+                        //                        }label: {
+                        //                            Image("Mobil2")
+                        //                                .resizable()
+                        //                                .scaledToFit()
+                        //                        }
+                        //                        .frame(width: geoMid.size.width / 12)
+                        //                        .background(.red)
+                        //                        .offset(x: geoMid.size.width / 3.45, y: geoMid.size.height / 2.25)
+                        
+                        Button{
+                            modalPresentMotor = true
+                        }label: {
+                            Image("Motor")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: geoMid.size.width / 11)
+                        .offset(x: geoMid.size.width / 3.45, y: geoMid.size.height / 2.25)
+                        .disabled(modalMotorAppended)
+                        .opacity(modalMotorAppended == true ? 0.5 : 1)
+                        
+                        Button{
+                            modalPresentMobil = true
+                        }label: {
+                            Image("Mobil1")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: geoMid.size.width / 11)
+                        .offset(x: geoMid.size.width / 2.6, y: geoMid.size.height / 1.9)
+                        .disabled(modalMobilAppended)
+                        .opacity(modalMobilAppended == true ? 0.5 : 1)
+                        
+                    }
+                }
+                .overlay{
+                    GeometryReader { geoBot in
+                        VStack{
+                            Spacer()
+                            
+                            HStack{
+                                Image("bayu-pilih-form")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geoBot.size.width / 8)
+                                    .offset(x: geoBot.size.width / 10, y: -geoBot.size.height / 9)
+                                
+                                Image("LaporHarta-07")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geoBot.size.width, height: geoBot.size.height / 6)
+                                    .offset(x: -geoBot.size.width / 4.5, y: -geoBot.size.height / 12)
+                                    .overlay{
+                                        Text("Yuk bantu Bayu daftarin hartanya. Klik setiap aset Bayu, lalu pilihlah data yang sesuai pada layar detail harta. Kemudian tekan tombol tambah.")
+                                            .font(.system(size: geoBot.size.width / 63, design: .rounded))
+                                            .bold()
+                                            .padding(.leading, geoBot.size.width / 8)
+                                            .padding(.trailing, geoBot.size.width / 1.8)
+                                            .offset(y: -geoBot.size.height / 11.4)
+                                    }
+                                Spacer()
+                            }
+                            //                            .background(.purple)
+                        }
+                    }
+                }
+                .overlay{
+                    VStack{
+//                        LazyVGrid(columns: gridItems, spacing: 10) {
+                            ForEach(listGambar, id: \.self) { imageName in
+                                Image(imageName)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: UIScreen.main.bounds.width / 4.3)
+                            }
+//                        }
+                        Spacer()
+                    }
+                    .frame(width: geoScreen.size.width * 0.24, height: geoScreen.size.height * 0.6)
+//                    .background(.red)
+                    .offset(x: geoScreen.size.width / 3.68, y: geoScreen.size.height / 10)
+                }
+        }
+        .fullScreenCover(isPresented: $modalPresentTanah, content: {
+            ReportAssetTanahView(modalPresentTanah: $modalPresentTanah, listGambar: $listGambar, modalTanahAppended: $modalTanahAppended)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.85)
+                .background(BackgroundClearView())
+        })
+        .fullScreenCover(isPresented: $modalPresentRumah, content: {
+            ReportAssetRumahView(modalPresentRumah: $modalPresentRumah, listGambar: $listGambar, modalRumahAppended: $modalRumahAppended)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.85)
+                .background(BackgroundClearView())
+        })
+        .fullScreenCover(isPresented: $modalPresentMobil, content: {
+            ReportAssetMobilView(modalPresentMobil: $modalPresentMobil, listGambar: $listGambar, modalMobilAppended: $modalMobilAppended)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.85)
+                .background(BackgroundClearView())
+        })
+        .fullScreenCover(isPresented: $modalPresentMotor, content: {
+            ReportAssetMotorView(modalPresentMotor: $modalPresentMotor, listGambar: $listGambar, modalMotorAppended: $modalMotorAppended)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.85)
+                .background(BackgroundClearView())
+        })
+        .ignoresSafeArea()
     }
+}
+
+struct BackgroundClearView: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
 }
 
 struct ReportAssetView_Previews: PreviewProvider {
     static var previews: some View {
         ReportAssetView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
